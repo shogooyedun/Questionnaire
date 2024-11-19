3ec1e910ef332f295bb3506a1d959f13c2af64ec
shogooyedun <segunoyedun@frootify.tech>
2024-11-18 19:23:20 -0800

    final task completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
 import React from 'react'
-import Questionaire from './components/Questionaire'
 import Questioniallash from './components/Questioniallash'
 
 const App = () => {
   return (
     <div>
-      {/* <Questionaire /> */}
       <Questioniallash/>
     </div>
   )
